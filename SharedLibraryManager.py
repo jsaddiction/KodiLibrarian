9d485a0cd07c897fdc665269ed249c99594e138c
jsaddiction <04reduramax@gmail.com>
2020-12-07 14:49:16 -0500

    'Pushing up changes with python selfupdate'

--- SharedLibraryManager.py
+++ SharedLibraryManager.py
@@ -1,10 +1,6 @@
-# import git
+#!/usr/bin/env python3
+
 import updates
 
 
 updates.update(verbose=True)
-# repo_path = updates.__find_repo()
-# repo = git.Repo(repo_path)
-
-# updates.__get_file_conflicts(repo, verbose=True)
-# updates.__get_file_diffs(repo, verbose=True)
