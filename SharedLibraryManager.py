ccb678213ddfb9bf82897ba001bf72e0301a8aa8
jsaddiction <04reduramax@gmail.com>
2020-12-07 14:48:04 -0500

    auto update

---
+++ SharedLibraryManager.py
@@ -0,0 +1,10 @@
+# import git
+import updates
+
+
+updates.update(verbose=True)
+# repo_path = updates.__find_repo()
+# repo = git.Repo(repo_path)
+
+# updates.__get_file_conflicts(repo, verbose=True)
+# updates.__get_file_diffs(repo, verbose=True)
