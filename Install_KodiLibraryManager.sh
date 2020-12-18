caabb19f1aa322de161bd6a3cce0539d829327a2
jsaddiction <04reduramax@gmail.com>
2020-12-18 11:29:22 -0500

    added install script

--- Install_KodiLibraryManager.sh
+++ Install_KodiLibraryManager.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#### USER SETTINGS ###
+# Install directory: can not be a root directory
+installDirectory="/scripts/KodiLibraryManager"
+
+### Scripts variables
+parentDir="$(dirname ${installDirectory})"
+requirementsFile="${installDirectory}/requirements.txt"
+
 apt update
 
 echo "********** INSTALLING GIT **********"
@@ -9,19 +17,26 @@ echo "********** INSTALLING PYTHON3 **********"
 apt install python3 -y
 apt install python3-pip -y
 
-### Clone KodiCoupler or update if already exists ###
-if [ ! -d "/config/scripts/KodiLibraryManager" ] 
+if [ ! -d ${parentDir} ]
+then
+    echo "*********** Creating parent script Directory ************"
+    error=$(mkdir -p ${parentDir} 2>&1) || { echo "Failed to create directory ${parentDir}. ERROR: ${error}" ; exit 1; }
+    error=$(chmod -R ug+rw ${parentDir} 2>&1) || { echo "Failed to set permissions on ${parentDir}. ERROR: ${error}" ; exit 1; }
+fi
+
+### Clone KodiLibraryManager or update if already exists ###
+if [ ! -d ${installDirectory} ]
 then
     echo "********** CLONING KODI_Library_Manager **********"
-    git clone https://github.com/jsaddiction/KodiLibraryManager.git /config/scripts/KodiCoupler
+    error=$(git clone https://github.com/jsaddiction/SharedLibraryManager.git ${installDirectory} 2>&1) || { echo "Failed to clone repo into ${installDirectory}. ERROR: ${error}" ; exit 1; }
 else
     echo "********** UPDATING KODI_Library_Manager **********"
-    git -C /config/scripts/KodiLibraryManager pull
+    error=$(git -C ${installDirectory} pull 2>&1) || { echo "Failed to update local repo. ERROR: ${error}" ; }
 fi
 
 ### Set permissions and install dependancies
 echo "********** Setting Permissions for KODI_Library_Manager **********"
-chmod -R ug+rwx /config/scripts/KodiLibraryManager
+error=$(chmod -R ug+rwx ${installDirectory} 2>&1) || { echo "Failed to set permissions on ${installDirectory}. ERROR: ${error}" ; exit 1;}
 
-echo "********** INSTALLING DEPENDANCIES **********"
-python3 -m pip install -r /config/scripts/KodiLibraryManager/requirements.txt
+echo "********** INSTALLING DEPENDENCIES **********"
+error=$(python3 -m pip install -r ${requirementsFile} 2>&1) || { echo "Failed to install dependencies. ERROR: ${error}" ; exit 1; }
