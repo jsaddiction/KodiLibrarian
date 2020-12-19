c4e42f4f5ebb194bc4ed42c85cd36dd142db4cba
jsaddiction <04reduramax@gmail.com>
2020-12-19 08:38:32 -0500

    update flow logic

--- KodiLibrarian.py
+++ KodiLibrarian.py
@@ -8,29 +8,29 @@ from librarian.librarian import Librarian
 
 updater.pull()
 log = logger.get_log('KodiLibrarian')
-# kodi = Librarian(config.hosts)
+kodi = Librarian(config.hosts)
 
-# log.debug('Environment Vars: {}'.format(env.allKnown))
+log.debug('Environment Vars: {}'.format(env.allKnown))
 
-# if env.event == 'download':
-#     if env.calledBy == 'radarr':
-#         log.info('Radarr has downloaded "{}" {}. Initiating update process.'.format(env.movieTitle, env.moviePath))
-#         kodi.updateMovie(env.movieTitle, env.movieDirectory, env.moviePath)
-#         if config.clean_after_update:
-#             kodi.cleanLibrary('movies')
+if env.event == 'download':
+    if env.calledBy == 'radarr':
+        log.info('Radarr has downloaded "{}" {}. Initiating update process.'.format(env.movieTitle, env.moviePath))
+        kodi.updateMovie(env.movieTitle, env.movieDirectory, env.moviePath)
+        if config.clean_after_update:
+            kodi.cleanLibrary('movies')
 
-#     elif env.calledBy == 'sonarr':
-#         log.info('Sonarr has downloaded "{}" {}. Initiating update process.'.format(env.showTitle, env.episodePath))
-#         kodi.updateTVShow(env.episodePath, env.showDirectory)
-#         if config.clean_after_update:
-#             kodi.cleanLibrary('tvshows')
+    elif env.calledBy == 'sonarr':
+        log.info('Sonarr has downloaded "{}" {}. Initiating update process.'.format(env.showTitle, env.episodePath))
+        kodi.updateTVShow(env.episodePath, env.showDirectory)
+        if config.clean_after_update:
+            kodi.cleanLibrary('tvshows')
 
-#     elif env.calledBy == 'lidarr':
-#         log.info('Lidarr not supported yet!! Aborting.')
+    elif env.calledBy == 'lidarr':
+        log.info('Lidarr not supported yet!! Aborting.')
 
-# elif env.event == 'test':
-#     log.debug('Called with test environment')
-#     sys.exit(0)
+elif env.event == 'test':
+    log.debug('Called with test environment')
+    sys.exit(0)
 
-# else:
-#     log.critical('Could not find any recognizable environment variables. Aborting.')
+else:
+    log.critical('Could not find any recognizable environment variables. Aborting.')
